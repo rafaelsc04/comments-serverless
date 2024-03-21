7a7279ea5aab5d16967c61bacf82acecb6447de3
Rafael Camargo <rafasc0404@gmail.com>
2024-03-21 01:21:54 -0300

    change linter to black

--- src/main.py
+++ src/main.py
@@ -1,32 +1,30 @@
-import boto3
-import json
-import uuid
+from boto3 import resource
+from json import loads, dumps
+from uuid import uuid4
 
-db = boto3.resource(
-    'dynamodb', endpoint_url='http://docker.for.mac.localhost:8000/')
-table = db.Table('comments')
+db = resource("dynamodb", endpoint_url="http://dynamodb-local:8000")
+table = db.Table("comments")
 
 
 def index():
     response = table.scan()
-    return {'statusCode': 200, 'body': json.dumps(response['Items'])}
+    return {
+        "statusCode": 200,
+        "body": dumps(response["Items"]),
+        "headers": {"content-type": "application/json"},
+    }
 
 
 def create(event):
-    data = json.loads(event['body'])
+    data = loads(event["body"])
 
-    table.put_item(
-        Item={
-            'uuid': str(uuid.uuid4()),
-            'comment': data['comment']
-        }
-    )
+    table.put_item(Item={"uuid": str(uuid4()), "comment": data["comment"]})
 
-    return {'statusCode': 200, 'body': json.dumps({'message': 'Created!'})}
+    return {"statusCode": 200, "body": dumps({"message": "Created!"})}
 
 
 def handler(event, context):
-    if event['httpMethod'] == 'GET':
+    if event["httpMethod"] == "GET":
         return index()
-    elif event['httpMethod'] == 'POST':
+    elif event["httpMethod"] == "POST":
         return create(event)
